bb5bd8eaa75cf54f0c701eed93d8c2bd8b0b410b
Joja81 <40449048+Joja81@users.noreply.github.com>
2020-04-27 22:04:43 +0800

    Fustration
    Created part of player selection screen however ran into problem with SetState not being able to change a list view widget. Found this at end of day. Will need to find alternate widget to use tomorrow

---
+++ up_and_down_the_river/lib/player.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class Player {
+  String name;
+  Color userColor;
+  int score;
+
+  Player(String n, Color c, int s) {
+    name = n;
+    userColor = c;
+    score = s;
+  }
+}
