05306c70a857b19e287b240754e30a354115eece
Joshua Smee <smeejosh81@gmail.com>
2020-06-05 20:44:09 +0800

    Added copyright notices

--- up_and_down_the_river/lib/main.dart
+++ up_and_down_the_river/lib/main.dart
@@ -4,12 +4,35 @@ import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:upanddowntheriver/startScreen.dart';
 
 void main() {
+  /*
+  The MIT License (MIT)
+
+Copyright (c) 2019 mobiten
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+   */
   runApp(Phoenix(
     //Phoenix allows the application to be restarted at the end of the game
     child: MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.blue,
-        accentColor: Colors.blueAccent,
+        primaryColor: Colors.lightGreen,
+        accentColor: Colors.lightGreenAccent,
       ),
       title: 'Up and down the river',
       home: StartScreen(),
