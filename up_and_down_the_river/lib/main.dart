e333d5cd248240a86ca3b7fb58c2ebef8b1d981c
Joshua Smee <smeejosh81@gmail.com>
2020-06-15 16:29:44 +0800

    Added comment

--- up_and_down_the_river/lib/main.dart
+++ up_and_down_the_river/lib/main.dart
@@ -8,17 +8,9 @@ void main() {
   List<Player> currentPlayers =
       setPlayer(); //Creates blank currentPlayers list to send to startScreen
   ThemeData initTheme = ThemeData.light();
-  /*
- Copyright (c) 2020 Kherel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-   */
   runApp(ThemeProvider(
-    //Allows switching of themes in the application
+    //Allows switching of themes in the application.
+    //Also allows the reativity of text to different text sizes defined within android, allows text to enlarge to match system settings, good for people with poor sight as text expands to accomodate.
     initTheme: initTheme, //Theme which is shown when the application is loaded
     child: Builder(builder: (context) {
       return MaterialApp(
